a6dc9d62f732a647595e6ca4631e11f8e4435f5c
nikul1ch <nikulin@netkam.ru>
2022-09-01 16:39:32 +0300

    Программа, которая находит разницу между максимальным и минимальным элементов массива

--- seminar5.3/Program.cs
+++ seminar5.3/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] arr = new double[n];
+
+void FillingArrayRandom(double[] array)
+{
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().NextDouble() * 10; //Умножил на 10 для наглядности
+}
+
+double MinDigitInArray(double[] array)
+{
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++) if (min > array[i]) min = array[i];
+    return min;
+}
+
+double MaxDigitInArray(double[] array)
+{
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++) if (max < array[i]) max = array[i];
+    return max;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.Write(array[i]);
+        else Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine(" ]");
+}
+
+FillingArrayRandom(arr);
+PrintArray(arr);
+
+double min = MinDigitInArray(arr);
+double max = MaxDigitInArray(arr);
+
+Console.Write($"Разница между максимальным и минимальным элементов массива = {max - min}");
